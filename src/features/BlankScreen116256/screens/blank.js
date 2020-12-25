8520a0ac2bf8a14ef4c46ba73d58cdc501c6553e
Crowdbotics <team@crowdbotics.com>
2020-12-25 07:44:12 +0000

    Update components

--- src/features/BlankScreen116256/screens/blank.js
+++ src/features/BlankScreen116256/screens/blank.js
@@ -40,6 +40,28 @@ export default class Blank extends React.Component {
         }}
         style={styles.Image_3}
       />
+      <View style={styles.View_4}>
+        <View />
+        <View>
+          <Image
+            source={{
+              uri:
+                "https://crowdbotics-slack-dev.s3.amazonaws.com/media/project_component_resources/2_Q1v8Fiy.jpg"
+            }}
+          />
+        </View>
+      </View>
+      <View>
+        <View>
+          <Image
+            source={{
+              uri:
+                "https://crowdbotics-slack-dev.s3.amazonaws.com/media/project_component_resources/2_Q1v8Fiy.jpg"
+            }}
+          />
+        </View>
+        <View />
+      </View>
     </View>
   )
 }
@@ -47,5 +69,13 @@ export default class Blank extends React.Component {
 const styles = StyleSheet.create({
   View_1: {},
   CheckBox_2: {},
-  Image_3: { width: 100, height: 100 }
+  Image_3: { width: 100, height: 100 },
+  View_4: { flexDirection: "row" },
+  View_5: {},
+  View_6: {},
+  Image_14: {},
+  View_7: {},
+  View_8: {},
+  Image_11: {},
+  View_9: {}
 })
