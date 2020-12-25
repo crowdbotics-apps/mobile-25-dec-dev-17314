714ea486fec21b56b91cd23a141bff6020d64447
Crowdbotics <team@crowdbotics.com>
2020-12-25 14:02:53 +0000

    Update components

--- src/features/BlankScreen116315/screens/blank.js
+++ src/features/BlankScreen116315/screens/blank.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from "react"
 import {
   View,
   Image,
@@ -10,32 +10,72 @@ import {
   TextInput,
   StyleSheet,
   ScrollView
-} from "react-native";
-import DateTimePicker from 'react-native-datepicker';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import Slider from '@react-native-community/slider';
-import { CheckBox } from 'react-native-elements';
-import {SlideMenuIcon} from '../../../navigator/slideMenuIcon';
+} from "react-native"
+import DateTimePicker from "react-native-datepicker"
+import Icon from "react-native-vector-icons/FontAwesome"
+import Slider from "@react-native-community/slider"
+import { CheckBox } from "react-native-elements"
+import { SlideMenuIcon } from "../../../navigator/slideMenuIcon"
 
 export default class Blank extends React.Component {
-  static navigationOptions = ({navigation}) => {
+  static navigationOptions = ({ navigation }) => {
     return {
-      headerLeft: <SlideMenuIcon navigationProps={navigation} />,
-    };
-  };
-  
-  state = {};
+      headerLeft: <SlideMenuIcon navigationProps={navigation} />
+    }
+  }
+
+  state = { CheckBox_2: true }
 
   render = () => (
-    <View style={styles.container}>
-      <Text>This is your new component</Text>
+    <View>
+      <CheckBox
+        title="Checkbox"
+        checked={this.state.CheckBox_2}
+        onPress={nextChecked => this.setState({ CheckBox_2: nextChecked })}
+      />
+      <Image
+        source={{
+          uri:
+            "https://crowdbotics-slack-dev.s3.amazonaws.com/media/project_component_resources/2_Q1v8Fiy.jpg"
+        }}
+        style={styles.Image_3}
+      />
+      <View style={styles.View_4}>
+        <View />
+        <View>
+          <Image
+            source={{
+              uri:
+                "https://crowdbotics-slack-dev.s3.amazonaws.com/media/project_component_resources/2_n4Hfy6f.jpg"
+            }}
+          />
+        </View>
+      </View>
+      <View>
+        <View>
+          <Image
+            source={{
+              uri:
+                "https://crowdbotics-slack-dev.s3.amazonaws.com/media/project_component_resources/2_Q1v8Fiy.jpg"
+            }}
+          />
+        </View>
+        <View />
+      </View>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginHorizontal: 16,
-  },
-});
+  View_1: {},
+  CheckBox_2: {},
+  Image_3: { width: 100, height: 100 },
+  View_4: { flexDirection: "row" },
+  View_5: {},
+  View_6: {},
+  Image_14: {},
+  View_7: {},
+  View_8: {},
+  Image_11: {},
+  View_9: {}
+})
